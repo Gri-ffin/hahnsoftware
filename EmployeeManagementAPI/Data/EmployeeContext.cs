4a3a6e0e84b662257e76ba393f40cda3e4c1d88f
Gri-ffin <gameshunter359@gmail.com>
2024-06-21 11:13:14 +0100

    create the data

---
+++ EmployeeManagementAPI/Data/EmployeeContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
